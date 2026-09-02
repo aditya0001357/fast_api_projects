7735f140a3b17375b440428fbacfd1bd740a383a
aditya0001357 <81770241+aditya0001357@users.noreply.github.com>
2026-09-02 18:18:10 +0000

    added crud operations

--- basics/6_crud_operations.py
+++ basics/6_crud_operations.py
@@ -1,5 +1,4 @@
-import sqlite3
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
 
@@ -35,7 +34,7 @@ app = FastAPI()
 
 @app.post('/todos')
 def create_todo(title: str, db: Session = Depends(get_database)):
-    todo = Todo(title, complete='False')
+    todo = Todo(title=title, completed='False')
     db.add(todo)
     db.commit()
     db.refresh(todo)
@@ -43,4 +42,54 @@ def create_todo(title: str, db: Session = Depends(get_database)):
         'message': 'todo created',
         'data': todo
     }
+
+
+@app.get('/todos')
+def get_all_todos(db: Session = Depends(get_database)):
+    all_todos = db.query(Todo).all()
+    return {
+        'message': f'These are all the {len(all_todos)} todos available so far.',
+        'data': all_todos
+    }
+
+
+@app.get('/todos/{todo_id}')
+def get_todo(todo_id: int, db: Session = Depends(get_database)):
     
+    try:
+        todo = db.query(Todo).filter(Todo.id == todo_id).one()
+    except:
+        raise HTTPException(status_code=404, detail='Todo not found.')
+
+    return todo
+
+
+@app.put('/todos/{todo_id}')
+def update_todo(todo_id: int, title: str, db: Session = Depends(get_database)):
+    try:
+        todo = db.query(Todo).filter(Todo.id == todo_id).one()
+    except:
+        raise HTTPException(status_code=404, detail='Todo not found.') 
+
+    todo.title = title
+
+    db.commit(); db.refresh(todo)
+    return {
+        'message': 'Updated the target todo.',
+        'data': todo
+    }   
+
+
+@app.delete('/todos/{todo_id}')
+def delete_todo(todo_id: int, db: Session = Depends(get_database)):
+    try:
+        todo = db.query(Todo).filter(Todo.id == todo_id).one()
+    except:
+        raise HTTPException(status_code=404, detail='Todo not found.') 
+
+    db.delete(todo)
+    db.commit()
+    return {
+        'message': 'Deleted the target todo.',
+        'data': todo
+    }
